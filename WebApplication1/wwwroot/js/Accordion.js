f706d21a0abc83e63d2e18b71c952d789a766c44
menna-ahmed-ibrahim <mibrahim@gemini.media>
2025-04-09 17:01:02 +0200

    editJs

--- WebApplication1/wwwroot/js/Accordion.js
+++ WebApplication1/wwwroot/js/Accordion.js
@@ -1,223 +1,126 @@
 ﻿var i = 0;
-var decimalRegax = /^-?\d*\.\d+$/;
-var integerRegax = /^-?\d+$/;
-var sum = 0;
-var sum2 = 0;
-var totat = 0;
+var sumRows = 0;
+var sumCols = 0;
+var total = 0;
+
 function addAccordion() { 
 
     console.log("add accordion ....");
     
     var accordionFlush = document.getElementById("accordionFlush");
 
+    //For design
     if (!accordionFlush.classList.contains('border') && !accordionFlush.classList.contains('border')) {
         accordionFlush.classList.add('border');
         accordionFlush.classList.add('rounded');
-    }
+    }   
+    var temp = document.getElementsByTagName("template")[0];
+    var newAccordionItem = temp.content.cloneNode(true);
+    var accordionNumber = newAccordionItem.querySelector(".accordion-number");  
+    accordionNumber.innerHTML += `${++i}`;
 
-    accordionFlush.innerHTML += `
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="faqHeading1">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#faqCollapse1" aria-expanded="true" aria-controls="faqCollapse1">
-        <span class="me-2 text-primary">
-          <i class="bi bi-question-circle-fill"></i>
-        </span>
-         Accordion Item #` + ++i +
-     ` 
-      </button>
-    </h2>
-    <div id="faqCollapse1" class="accordion-collapse collapse show" aria-labelledby="faqHeading1" data-bs-parent="#faqAccordion">
-      <div class="accordion-body">
-              <button type="button" class="btn btn-danger" onclick="addRow(event)" id="btn-`+ i +`">Add Row</button>
-             
-                <table class="table" id="table-`+i+`" >
-                  <thead class="thead-dark">
-                   <tr>
-                    <th scope="col" class="text-center">Col1</th>
-                    <th scope="col" class="text-center">Col2</th>
-                    <th scope="col" class="text-center">Col3</th>
-                    <th scope="col" class="text-center">Sum</th>
-                   </tr>
-                  </thead>
-                 <tbody id = "tbody">            
-                 </tbody>
-                 <tfoot id="footer">
-                   <tr>
-                    <td><input class="form-control sum col-1" type="number" value="0" readonly></td>
-                    <td><input class="form-control sum col-2" type="number" value="0" readonly></td>
-                    <td><input class="form-control sum col-3" type="number" value="0" readonly></td>
-                    <td><input class="form-control col-4" type="number" value="0"  id="total" readonly></td>
-                   </tr>
-                 </tfoot>
-               </table>           
-       </div>
-     </div>
-   </div>
-   `
+    var addBtn = newAccordionItem.querySelector(".add");
+    
+
+    var table = newAccordionItem.querySelector(".table");
+    table.id += i;
+
+    accordionFlush.appendChild(newAccordionItem); 
 
 }
 
-function addRow(e) {
-    var tbody = document.getElementById("tbody");
-    tbody.innerHTML +=
-    `
-    <tr>
-    <td> <input class="form-control col-1" type="number" value="0" onchange="decimalInputValidate1(this)"><span class="Error input-Error d-none text-danger" style="font-size: 14px;"></span></td>
-    <td> <input class="form-control col-2" type="number" value="0" onchange="integerInputValidate(this)"> <span class="Error input-Error d-none text-danger" style="font-size: 14px;"></span> </td>
-    <td> <input class="form-control col-3" type="number" value="0" onchange="decimalInputValidate2(this)"> <span class="Error input-Error d-none text-danger" style="font-size: 14px;"></span> </td>
-    <td> <input class="form-control sumRow col-4" type="number" value="0" readonly></td>
-    </tr>
-    `
+function addRow(btn) {
+    var tbody = btn.parentElement.querySelector("#tbody");
+    var temp2 = document.getElementsByTagName("template")[1];
+    var newRow = temp2.content.cloneNode(true);
+    tbody.appendChild(newRow);
     
 }
 
+ 
 
-function decimalValidation(inputEle) {
-    var td = inputEle.closest("td");
-    var spanElement = td.querySelector(".input-Error");
-
-    if (!decimalRegax.test(Number(inputEle.value))) {
-        inputEle.classList.add("border-danger")
-
-        var spanElement = td.querySelector(".input-Error");
-        spanElement.classList.remove("d-none");
-        spanElement.classList.add("d-block");
-        spanElement.innerHTML = "Please enter a decimal number";
-        return false;
+document.getElementsByClassName("accordion-flush")[0].addEventListener("input", function (event) {
+    if (event.target && event.target.matches(".integer")) {
+        var integerInput = event.target;
+        //console.log(integerInput);
+        if (integerInput.value %  1) {
+           integerInput.value = Math.trunc(integerInput.value);
+        }
     }
-    else {
-        inputEle.classList.remove("border-danger");
-        spanElement.classList.remove("d-block");
-        spanElement.classList.add("d-none");
-        return true;
-    }
-}
+    if (event.target && event.target.matches(".valnum")) {
+        // sum row
+        var row = event.target.closest("tr").querySelectorAll(".valnum");
+        sumRows = 0;
+        sumCols = 0;
+   
+        row.forEach(sumRow);
+        var sumCell = event.target.closest("tr").querySelector(".sum");     
+        sumCell.value = sumRows;
 
-function decimalInputValidate1(inputElement) {
-    if (decimalValidation(inputElement)) {
-        var tr = inputElement.parentElement.parentElement;
-        console.log(tr);
-        var Rowinputs = tr.querySelectorAll("input");
-        var sumInp = tr.querySelector(".sumRow");
-        sum = 0;
-        Rowinputs.forEach(sumRow);
-        sumInp.value = sum;
-
-
-        var tbody = tr.parentElement;
-        console.log(tbody);
-        var colInputs = tbody.querySelectorAll(".col-1");
-        console.log(colInputs);
-        var tfoot = document.getElementById("footer");
-        var colSumInput = tfoot.querySelector(".col-1");
-        sum2=0
-        colInputs.forEach(sumCol);
-        colSumInput.value = sum2;
-
-        var totalInput = document.getElementById("total");
-        console.log(totalInput);
+        //total
         total = 0;
-        Calctotal();
-        totalInput.value = total
+        var totalCell = event.target.closest(".collapse").querySelector(".total")
+        var sumsInputs = event.target.closest(".collapse").querySelectorAll(".sumRow");
+        console.log("TotalCell");
+        console.log(totalCell);
+        sumsInputs.forEach(inp => {
+            total += Number(inp.value);
+        })
+        totalCell.value = total;
+        //sum column
+        var rowArray = Array.from(row)
+        var index = rowArray.indexOf(event.target);
         
-    };
-}
+
+        var tbody = event.target.closest(".table").querySelector("tbody");
+        var rows = tbody.querySelectorAll("tr");
+        
+        console.log(rows)
+        rows.forEach(row => {
+            var tds = row.querySelectorAll("td");
+             
+            console.log(tds);
+           
+
+            tds.forEach(td => {
+                console.log(Array.from(tds).indexOf(td))
+                if (Array.from(tds).indexOf(td) === index) {
+                    sumCol(td.querySelector(".valnum"));
+
+                    var tfooter = event.target.closest("table").querySelector("#footer");
+                    console.log("FOOTER");
+                    console.log(tfooter);
+                    var footerRow = tfooter.querySelector("tr");
+                    var footerTds = footerRow.querySelectorAll("td");
+
+                    footerTds.forEach(td => {
+                        if (Array.from(footerTds).indexOf(td) === index) {
+                            td.querySelector(".sum").value = sumCols;
+                        }
+
+                    });
 
 
-function decimalInputValidate2(inputElement) {
-    if (decimalValidation(inputElement)) {
-        var tr = inputElement.parentElement.parentElement;
-        console.log(tr);
-        var Rowinputs = tr.querySelectorAll("input");
-        var sumInp = tr.querySelector(".sumRow");
-        sum = 0;
-        Rowinputs.forEach(sumRow);
-        sumInp.value = sum;
+                };
+            });
 
+        });
+     }
 
-        var tbody = tr.parentElement;
-        console.log(tbody);
-        var colInputs = tbody.querySelectorAll(".col-3");
-        console.log(colInputs);
-        var tfoot = document.getElementById("footer");
-        var colSumInput = tfoot.querySelector(".col-3");
-        sum2 = 0
-        colInputs.forEach(sumCol);
-        colSumInput.value = sum2;
-
-        var totalInput = document.getElementById("total");
-        total = 0;
-        Calctotal();
-        totalInput.value = total;
-    }
-}
-
-
-function integerInputValidate(inputElement) {
-    var td = inputElement.closest("td");
-    var spanElement = td.querySelector(".input-Error");
-
-    if (!integerRegax.test(Number(inputElement.value))) {
-        inputElement.classList.add("border-danger")
-
-        var spanElement = td.querySelector(".input-Error");
-        spanElement.classList.remove("d-none");
-        spanElement.classList.add("d-block");
-        spanElement.innerHTML = "Please enter an integer number";
-
-    }
-    else {
-        inputElement.classList.remove("border-danger");
-        spanElement.classList.remove("d-block");
-        spanElement.classList.add("d-none");
-           var tr = inputElement.parentElement.parentElement;
-        console.log(tr);
-        var inputs = tr.querySelectorAll("input");
-        var sumInp = tr.querySelector(".sumRow");
-        sum = 0;
-        inputs.forEach(sumRow);
-        sumInp.value = sum;
-
-
-        var tbody = tr.parentElement;
-        console.log(tbody);
-        var colInputs = tbody.querySelectorAll(".col-2");
-        console.log(colInputs);
-        var tfoot = document.getElementById("footer");
-        var colSumInput = tfoot.querySelector(".col-2");
-        sum2 = 0
-        colInputs.forEach(sumCol);
-        colSumInput.value = sum2;
-
-        var totalInput = document.getElementById("total");
-        total = 0;
-        Calctotal();
-        totalInput.value = total
-    }
-}
-
+    
+});
 
 function sumRow(input) {
-    //console.log(input);
-    if (!input.classList.contains("sumRow")) {
-       sum += Number(input.value);
-    }
-}
-
-
-function sumCol(input) {
-    //console.log(input);
     if (!input.classList.contains("sum")) {
-        sum2 += Number(input.value);
+        sumRows += Number(input.value);
     }
 }
 
-function Calctotal() {
-    var sumInputs = document.querySelectorAll(".sumRow");
-    sumInputs.forEach(input => {
-        total += Number(input.value);
-    })
+function sumCol(input) { 
+    sumCols += Number(input.value);
+    console.log("sumcols" + sumCols);
 }
-
 
  
+
+
